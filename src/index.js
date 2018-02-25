9b86142fec36f947b7951bb04ccb2798476c8cf7
MatusevichAlexey <alexeimatusevich97@gmail.com>
2018-02-25 17:48:00 +0300

    implement getZerousCount function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var result = 0;
+  for (var i = 1; i <= Math.floor(Math.pow(number, 0.2)); i++) {
+    result = result + Math.floor(number/Math.pow(5,i));
+  }
+  return result;
 }
